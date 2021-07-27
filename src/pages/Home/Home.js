6f7d728d75014fead781a6dd8bc0c8e574723c67
UjjwalSingh1908 <ujjwal1911044@akgec.ac.in>
2021-07-28 01:31:22 +0530

    carousel, footer

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import ImgCarousel from '../../components/carousel/Carousel'
+import Footer from '../../components/footer/Footer'
 import Header from '../../components/Header/Header'
 
 function Home() {
   return (
     <>
     <Header/>
+    <ImgCarousel />
+    <Footer />
     </>
   );
 }
